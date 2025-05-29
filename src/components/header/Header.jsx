a3671a531c6adfd668710b10a267cb334a69f7c9
bokhodir-sultonmurodov <boxodirsultanmurodov@gmail.com>
2025-05-29 17:09:12 +0500

    Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="bg-gradient-to-r from-gray-800 to-gray-600 text-white shadow-md fixed top-0 left-0 w-full z-20">
       <nav className="container mx-auto flex justify-between items-center px-4 py-3">
         <NavLink to="/" className="text-2xl font-bold">
-          MyApp
+         FreeApi
         </NavLink>
 
         <div className="hidden md:flex gap-8 items-center text-sm">
